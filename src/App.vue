ce308101105139adbac80eb88590d844b2bb9b0e
Агаев Даниэл Сейранович <gr402_agdse@ttit.local>
2022-05-03 11:51:43 +0700

    start

--- src/App.vue
+++ src/App.vue
@@ -89,6 +89,55 @@
           d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 24.984375 6.9863281 A 1.0001 1.0001 0 0 0 24 8 L 24 22.173828 A 3 3 0 0 0 22 25 A 3 3 0 0 0 22.294922 26.291016 L 16.292969 32.292969 A 1.0001 1.0001 0 1 0 17.707031 33.707031 L 23.708984 27.705078 A 3 3 0 0 0 25 28 A 3 3 0 0 0 28 25 A 3 3 0 0 0 26 22.175781 L 26 8 A 1.0001 1.0001 0 0 0 24.984375 6.9863281 z"
         ></path>
       </svg>
+
+      <e-calendar
+        :data="[
+          {
+            fontFamily: 'Raleway',
+            fontSize: '14px',
+            fontWeight: 'regular',
+            activeColor: '#0066FF',
+            activeBackgroundColor: '#E5F0FF',
+            isAdaptiveSize: false,
+            isDouble: true,
+            locale: 'ru-RU',
+            timePicker: {
+              isAMPM: true,
+              label: 'time 1',
+            },
+            data: {
+              date_from: '2022-03-02T04:28:12.089Z',
+              date_to: '2022-03-05',
+            },
+          },
+          {
+            fontFamily: 'Raleway',
+            fontSize: '14px',
+            fontWeight: 500,
+            activeColor: '#51e',
+            activeBackgroundColor: '#E5F0FF',
+            isAdaptiveSize: false,
+            isDouble: false,
+            locale: 'en-US',
+            timePicker: {
+              isAMPM: false,
+              label: 'time 2',
+            },
+            data: {
+              date_from: '2022-03-02',
+            },
+          },
+          {
+            fontFamily: 'Open Sans',
+            fontSize: '18px',
+            activeColor: 'red',
+            activeBackgroundColor: '#ffF0FF',
+            isAdaptiveSize: false,
+            data: {},
+          },
+        ]"
+      ></e-calendar>
+
       <input
         class="details-modal-edit-inputs details-modal-edit-date"
         type="date"
